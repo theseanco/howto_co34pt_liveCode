1c17fa98684b315273b90b5ccf4979f5f69cb33a
theseanco <theseancotterill@live.com>
2017-09-28 14:50:55 +0100

    added examples for live data and static data

--- 6: Visuals and Data/6.2: OSC and Data Streams/6.2: OSC and Data Streams - Example Files/python_randomNumber.py
+++ 6: Visuals and Data/6.2: OSC and Data Streams/6.2: OSC and Data Streams - Example Files/python_randomNumber.py
@@ -3,13 +3,24 @@ import random
 import time
 import OSC
 
+# Connect to SuperCollider's internal port
 c = OSC.OSCClient()
 c.connect(('127.0.0.1', 57120))
 
-
+# Repeatedly send random messages which will be turned into a Warp1 Ugen pointer in ProxySpace
 while True:
     oscmsg = OSC.OSCMessage()
     oscmsg.setAddress("/warpPointer")
     oscmsg.append(random.random())
     c.send(oscmsg)
-    time.sleep(random.uniform(0.1, 10))
+    time.sleep(random.uniform(0.1, 1))
+    oscmsg = OSC.OSCMessage()
+    oscmsg.setAddress("/warpRate")
+    oscmsg.append(random.uniform(0.1,3))
+    c.send(oscmsg)
+    time.sleep(random.uniform(0.1,1))
+    oscmsg = OSC.OSCMessage()
+    oscmsg.setAddress("/warpWindow")
+    oscmsg.append(random.uniform(0.01,0.9))
+    c.send(oscmsg)
+    time.sleep(random.uniform(0.1,1))
